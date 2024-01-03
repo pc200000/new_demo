ffc0bb5e141795e539115c928b15275087417e75
pc200000 <pc200000@qq.com>
2024-01-03 14:54:53 +0800

    add test case
    add test case

--- demo/src/test/java/com/scores/ScoresServiceTest.java
+++ demo/src/test/java/com/scores/ScoresServiceTest.java
@@ -1,8 +1,40 @@
 package com.scores;
 
+import com.scores.model.Student;
+import com.scores.service.ScoresService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 public class ScoresServiceTest {
 
+	@Autowired
+	private ScoresService scoresService;
+
+	@Test
+	public void testGetStudentList(String classId) {
+		// 调用 getStudentList 方法获取学生列表
+		List<Student> studentList = scoresService.getStudents(classId);
+
+		// 断言学生列表不为空
+		Assertions.assertNotNull(studentList);
+		// 断言学生列表中至少有一个学生
+        Assertions.assertFalse(studentList.isEmpty());
+	}
+
+	@Test
+	public void testScoresServiceMethods(String classId) {
+		List<Student> studentList = scoresService.getStudents(classId);
+		// 调用 calculateMedian 方法计算中位数
+		double median = scoresService.calMedian(studentList);
+		Assertions.assertNotSame(Double.NaN, median);
+
+		// 调用 calculateAverage 方法计算平均值
+		double average = scoresService.calAvg(studentList);
+		Assertions.assertNotSame(Double.NaN, average);
+	}
 }
